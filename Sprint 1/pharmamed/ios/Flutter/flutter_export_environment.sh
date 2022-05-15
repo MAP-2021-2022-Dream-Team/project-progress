0e21e83568609d8fa9712128fba6490cbaa1aaf3
IrtisumMusharratRahman <rhythmfakir@gmail.com>
2022-05-16 02:18:07 +0800

    .

--- Sprint 1/pharmamed/ios/Flutter/flutter_export_environment.sh
+++ Sprint 1/pharmamed/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\rhyth\Desktop\Sprint 1\pharmamed"
+export "FLUTTER_APPLICATION_PATH=C:\Users\rhyth\Desktop\project\project-progress\Sprint 1\pharmamed"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
